fc21e0bf81d5c762cc0425d8cf048664f4df19c9
Your Name <you@example.com>
2015-02-18 18:17:31 +0100

    Updating 006

--- static/js/script.js
+++ static/js/script.js
@@ -4,3 +4,26 @@
 				jQuery('#menu').slideToggle();
 			});
 		});
+
+
+
+    $(document).ready(function() {
+     
+    $("#owl-example").owlCarousel({
+     
+     navigation : true, // Show next and prev buttons
+slideSpeed : 300,
+paginationSpeed : 400,
+singleItem:true
+ 
+// "singleItem:true" is a shortcut for:
+// items : 1,
+// itemsDesktop : false,
+// itemsDesktopSmall : false,
+// itemsTablet: false,
+// itemsMobile : false
+ 
+});
+ 
+});
+
